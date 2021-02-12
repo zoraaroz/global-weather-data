a578a1a9e5f787994ccdf61d1273b05ead8f88e0
zoraaroz <zschaerer@mac.com>
2021-02-12 14:07:51 +0100

    code: Add query for city data

--- queries.sql
+++ queries.sql
@@ -9,3 +9,16 @@ AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 8 PRECEDING AND CURRENT ROW) AS m
 AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS moving_year_avg_10
 FROM global_data
 GROUP BY year, avg_temp;
+
+SELECT year, avg_temp,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 2 PRECEDING AND CURRENT ROW) AS moving_year_avg_3,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 3 PRECEDING AND CURRENT ROW) AS moving_year_avg_4,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 4 PRECEDING AND CURRENT ROW) AS moving_year_avg_5,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 5 PRECEDING AND CURRENT ROW) AS moving_year_avg_6,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 6 PRECEDING AND CURRENT ROW) AS moving_year_avg_7,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 7 PRECEDING AND CURRENT ROW) AS moving_year_avg_8,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 8 PRECEDING AND CURRENT ROW) AS moving_year_avg_9,
+AVG(avg_temp) OVER (ORDER BY year ROWS BETWEEN 9 PRECEDING AND CURRENT ROW) AS moving_year_avg_10
+FROM city_data
+WHERE city IN ('Berlin')
+GROUP BY year, avg_temp;
